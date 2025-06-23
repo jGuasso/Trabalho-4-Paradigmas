67a75448ff65cdf02ccddcc4027d07e14fc28068
jGuasso <joao.guasso@acad.ufsm.br>
2025-06-23 09:57:11 -0300

    ajuste na qquestão 5

--- src/questao5.java
+++ src/questao5.java
@@ -4,10 +4,10 @@ public class questao5 {
     public static void main(String[] args) {
         TreeMap<String, Double> arvores = new TreeMap<>();
         arvores.put("Angico-Vermelho", 37.5);
-        arvores.put("Araçá-Amarelo", 4.5);
-        arvores.put("Canela-Sassafrás", 20.0);
         arvores.put("Guajuvira", 13.5);
         arvores.put("Pau-Ferro", 12.5);
+        arvores.put("Araçá-Amarelo", 4.5);
+        arvores.put("Canela-Sassafrás", 20.0);
 
         for (Map.Entry<String, Double> entrada : arvores.entrySet()) {
             System.out.printf("%-16s - %.1f metros%n", entrada.getKey(), entrada.getValue());
